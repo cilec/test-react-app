0c89fa209091957f0600f922196adf0e0f0c3343
cc <19948031@qq.com>
2017-11-14 23:21:13 +0800

    增加登录注册

--- src/components/PCHeader.jsx
+++ src/components/PCHeader.jsx
@@ -1,15 +1,50 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom'
 import logoImg from '../img/logo.png';
-import {Row, Col} from 'antd/lib/grid';
-import {Menu, Icon} from 'antd';
-export default class PCHeader extends Component {
+import {
+    Menu,
+    Icon,
+    Row,
+    Col,
+    Input,
+    Tabs,
+    message,
+    Button,
+    Checkbox,
+    Form
+} from 'antd';
+const FormItem = Form.Item;
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
+class PCHeader extends Component {
     constructor() {
         super();
         this.state = {
-            current: 'top'
+            current: 'top',
+            modalVisible: false,
+            action: 'login',
+            hasLogined: false,
+            userNickName: '',
+            userid: 0
         }
     }
     render() {
+        let {getFieldProps} = this.props.form;
+        const userShow = this.state.hasLogined
+            ? <Menu.Item key="logout" className="register">
+                    <Button type="primary" htmlType="button">{this.state.userNickName}</Button>
+                    &nbsp;&nbsp;
+                    <Link target="_blank">
+                        <Button type="dashed" htmlType="button">个人中心</Button>
+                    </Link>
+                    &nbsp;&nbsp;
+                    <Link target="_blank">
+                        <Button type="ghost" htmlType="button">退出</Button>
+                    </Link>
+                </Menu.Item>
+            : <Menu.Item key="register" class="register">
+                <Icon type="appstore"></Icon>注册/登录
+            </Menu.Item>
         return (
             <header>
                 <Row>
@@ -46,6 +81,7 @@ export default class PCHeader extends Component {
                             <Menu.Item key="fashion">
                                 <Icon type="appstore"/>时尚
                             </Menu.Item>
+                            {userShow}
                         </Menu>
                     </Col>
                     <Col span={2}></Col>
@@ -55,3 +91,4 @@ export default class PCHeader extends Component {
         )
     }
 }
+export default PCHeader = Form.create()(PCHeader)
