c8a7371e07f34a3edfc171931676e15f9767cc54
cc <19948031@qq.com>
2017-11-19 17:13:01 +0800

    修改样式

--- src/components/PCNewsContainer.jsx
+++ src/components/PCNewsContainer.jsx
@@ -15,7 +15,7 @@ export default class PCNewsContainer extends React.Component {
             <div>
                 <Row>
                     <Col span={2}></Col>
-                    <Col span={20} className="container">
+                    <Col span={16} className="container">
                         <div className="leftContainer">
                             <div className="carousel">
                                 <Carousel {...settings}>
