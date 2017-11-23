d88c66574ef9ec91bd3c1bbf3464e574ca1d63f8
cc <19948031@qq.com>
2017-11-23 16:41:44 +0800

    左边图片新闻列表开发

--- src/components/PCImgNewsBlock.jsx
+++ src/components/PCImgNewsBlock.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Card } from 'antd';
-import { Route, Router } from 'react-router'
-import { Link, BrowserRouter } from 'react-router-dom';
+import {Card} from 'antd';
+import {Route, Router} from 'react-router'
+import {Link, BrowserRouter} from 'react-router-dom';
 
 export default class PCImgNewsBlock extends React.Component {
     constructor() {
@@ -12,53 +12,63 @@ export default class PCImgNewsBlock extends React.Component {
     }
     componentDidMount() {
         let myFetchOptions = {
-            method: 'GET',
+            method: 'GET'
         };
         console.log('component will mount')
         fetch(`http://newsapi.gugujiankong.com/Handler.ashx?action=getnews&type=${this.props.type}&count=${this.props.count}`, myFetchOptions).then(response => {
             return response.text()
         }).then(result => {
             let json = JSON.parse(result);
-            this.setState({ news: json })
+            this.setState({news: json})
         })
     }
     render() {
         const styleImage = {
-            display: "block",
-            width: this.props.imageWidth,
-            height: '90px'
-        },
+                display: "block",
+                width: this.props.imageWidth,
+                height: '90px',
+                maxWidth:'100%'
+            },
             styleH3 = {
                 width: this.props.imageWidth,
                 whiteSpace: 'nowrap',
                 overflow: 'hidden',
                 textOverflow: 'ellipsis'
             };
-        const { news } = this.state;
+        const {news} = this.state;
         const newsList = news.length
-            ?
-            news.map((newsItem, index, arr) => {
+            ? news.map((newsItem, index, arr) => {
                 // console.log(arr)
-                return <li key={index}>
+                return <li
+                    key={index}
+                    style={{
+                    listStyleType: "none",
+                    width:"112px"
+                }}>
                     <Link to={`details/${newsItem.uniquekey}`} target="_blank">
                         <div className="custom-image">
-                            <img alt="" style={styleImage} src={newsItem.thumbnail_pic_s} />
+                            <img alt="" style={styleImage} src={newsItem.thumbnail_pic_s}/>
                         </div>
                         <div className="custom-title">
-                            <h3 style={styleH3}>{newsItem.title}</h3>
+                            <h5 style={styleH3}>{newsItem.title}</h5>
                             <p>{newsItem.author_name}</p>
                         </div>
                     </Link>
                 </li>;
             })
-            :
-            "没有加载到任何新闻";
-        // console.log(this.state.news)
-        // console.log('newsList', newsList)
+            : "没有加载到任何新闻";
+        // console.log(this.state.news) console.log('newsList', newsList)
         return (
             <div className="topNewsList">
-                <Card title={this.props.cartTitle} bordered style={{ width: this.props.width }}>
-                    {newsList}
+                <Card
+                    title={this.props.cartTitle}
+                    bordered
+                    style={{
+                    width: this.props.width
+                }}>
+                    <ul className="news-img-list">
+                        {newsList}
+                    </ul>
                 </Card>
             </div>
         )
--- src/components/PCNewsContainer.jsx
+++ src/components/PCNewsContainer.jsx
@@ -26,7 +26,7 @@ export default class PCNewsContainer extends React.Component {
                             <div><img src={require('../img/carousel/3.jpeg')} /></div>
                             <div><img src={require('../img/carousel/4.jpeg')} /></div>
                         </Carousel>
-                        <PCImgNewsBlock count={6} type="guoji" width="400px" cartTitle="国际头条" imageWidth="100px" />
+                        <PCImgNewsBlock count={6} type="guoji" width="100%"  cartTitle="国际头条" imageWidth="100%" />
                     </Col>
                     {/* </div>
                         </div> */}
