87a1751c815adcc41e96adfed47cdc7052e61f63
cc <19948031@qq.com>
2017-11-24 17:15:12 +0800

    添加下部图片新闻栏

--- src/components/PCIndex.jsx
+++ src/components/PCIndex.jsx
@@ -4,12 +4,14 @@ import '../css/pc.css'
 import PCFooter from './PCFooter';
 import PCNewsContainer from './PCNewsContainer'
 import { Router, BrowserRouter } from 'react-router-dom';
+import PCImgNewsBlock from './PCImgNewsBlock'
 export default class PCIndex extends React.Component {
     render() {
         return (
             <div>
                 <PCHeader></PCHeader>
                 <PCNewsContainer />
+                <PCImgNewsBlock count={6} type="guonei" width="100%" cartTitle="国内头条" imageWidth="100%" />
                 <PCFooter></PCFooter>
             </div>
         )
