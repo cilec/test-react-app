261731b00a99032f9a9d7fc784154fbb2bebbbc6
cc <19948031@qq.com>
2017-11-30 23:14:35 +0800

    详情页添加页头页脚

--- src/components/PCNewsDetails.jsx
+++ src/components/PCNewsDetails.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import {Row, Col} from 'antd/lib/grid';
+import PCHeader from './PCHeader';
+import PCFooter from './PCFooter';
 export default class PCNewsDetails extends React.Component {
     constructor() {
         super();
@@ -12,7 +14,8 @@ export default class PCNewsDetails extends React.Component {
             method: 'GET'
         }
         // console.log(this.props.match)
-             //http://newsapi.gugujiankong.com/Handler.ashx?action=getnewsitem&uniquekey=161028202106247
+        // http://newsapi.gugujiankong.com/Handler.ashx?action=getnewsitem&uniquekey=161
+        // 0 28202106247
         fetch(`http://newsapi.gugujiankong.com/Handler.ashx?action=getnewsitem&uniquekey=${this.props.match.params.uniquekey}`, myFetchOptions).then(response => {
             return response.text()
         }).then(res => {
@@ -29,6 +32,7 @@ export default class PCNewsDetails extends React.Component {
     render() {
         return (
             <div>
+                <PCHeader></PCHeader>
                 <Row>
                     <Col span={2}></Col>
                     <Col span={14} className="pc_details_container">
@@ -37,6 +41,7 @@ export default class PCNewsDetails extends React.Component {
                     <Col span={6}></Col>
                     <Col span={2}></Col>
                 </Row>
+                <PCFooter></PCFooter>
             </div>
         )
 
